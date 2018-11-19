734bb1852fc07833b6d4adfe8e6851c023d07c0f
Diab Jerius <djerius@cfa.harvard.edu>
2018-11-18 23:28:47 -0500

    fix 'is' parameters

--- examples/config_file/lib/MyCmd.pm
+++ examples/config_file/lib/MyCmd.pm
@@ -4,14 +4,14 @@ use Moo;
 use CLI::Osprey;
 
 option 'message' => (
-    is      => 'lazy',
+    is      => 'ro',
     format  => 's',
     doc     => 'The message to display',
     default => 'Hello World',
 );
 
 option 'config' => (
-    is     => 'rw',
+    is     => 'ro',
     format => 's',
     short  => 'f',
     doc    => 'config file',
